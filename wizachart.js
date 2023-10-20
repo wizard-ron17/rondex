705b7a2dec0b1f5c582588722f41a1a82c88ebc0
wizard-ron17 <128203911+wizard-ron17@users.noreply.github.com>
2023-10-20 16:09:46 -0400

    Update wizachart.js

--- wizachart.js
+++ wizachart.js
@@ -98,6 +98,9 @@ var options = {
         data: dataPoints,
     }],
     chart: {
+        toolbar: {
+            show: true
+        },
         foreColor: 'white',
         type: 'candlestick',
         height: 500
