70a77f5b874df57c4ef0e9fe1fce979978895d1e
ron <128203911+wizard-ron17@users.noreply.github.com>
2024-12-01 20:44:08 -0500

    Update wiza-chart.js

--- wiza/wiza-chart.js
+++ wiza/wiza-chart.js
@@ -22,7 +22,7 @@ async function fetchPriceData(period) {
         }`;
 
     try {
-        const response = await fetch('https://kdswap-fd-prod-cpeabrdfgdg9hzen.z01.azurefd.net/graphql', {
+        const response = await fetch('https://api.kdswap.exchange/graphql', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
